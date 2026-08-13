71707d917bcb56b9afda723b1249a5a421095db9
vamsi <bathuyuvaraj@gmail.com>
2026-08-13 14:36:30 +0530

    ✅ BUDTECH - Rating 449 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-13

---
+++ codechef/easy/BUDTECH---Rating-449/solution.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        for (int i = 0; i < t; i++) {
+            int r = scanner.nextInt();
+            System.out.println(r * 100);
+        }
+    }
+}
