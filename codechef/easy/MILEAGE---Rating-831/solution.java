9881bf84e967836426ee93d2fa8f1feba1be2eb3
vamsi <bathuyuvaraj@gmail.com>
2026-08-13 15:16:32 +0530

    ✅ MILEAGE - Rating 831 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-13

---
+++ codechef/easy/MILEAGE---Rating-831/solution.java
@@ -0,0 +1,33 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+    public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc=new Scanner(System.in);
+    	int t=sc.nextInt();
+    	while(t-->0){
+    	    double n=sc.nextInt();
+    	    double x= sc.nextInt();
+    	    double y=sc.nextInt();
+    	    double a=sc.nextInt();
+    	    double b= sc.nextInt();
+    	    
+    	    double p = (n/a)*x;
+    	    double d= (n/b)*y;
+    	    
+    	    if(p>d){
+    	        System.out.println("DIESEL");
+    	    }else if (p<d){
+    	        System.out.println("PETROL");
+    	    }else{
+    	        System.out.println("ANY");
+    	    } 
+    	    
+    	    
+    	}
+	}
+}
