fd980c892997acfa4f108955113df30d37dab9cd
vamsi <bathuyuvaraj@gmail.com>
2026-08-14 11:23:25 +0530

    ✅ IMDB - Rating 1098 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-14

---
+++ codechef/easy/IMDB---Rating-1098/solution.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner in=new Scanner(System.in);
+		int t=in.nextInt();
+		for(int i=0;i<t;i++)
+		{
+		    int n=in.nextInt();
+		    int x=in.nextInt();
+		    int maxs=1;
+		    for(int j=0;j<n;j++)
+		    {
+		        int s=in.nextInt();
+		        int r=in.nextInt();
+		        if(s<=x)
+		        {
+		           maxs=Math.max(maxs,r); 
+		        }
+		    }System.out.println(maxs);
+		}
+	}
+}
