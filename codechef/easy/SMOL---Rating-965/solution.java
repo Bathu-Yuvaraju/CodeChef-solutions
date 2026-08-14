92a0bc67cc92cc54c162bc6ec26c312f69df0aac
vamsi <bathuyuvaraj@gmail.com>
2026-08-14 11:27:33 +0530

    ✅ SMOL - Rating 965 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-14

---
+++ codechef/easy/SMOL---Rating-965/solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+		    int[] a=new int[3];
+		    a[0]=sc.nextInt();
+		    a[1]=sc.nextInt();
+		    a[2]=sc.nextInt();
+		    Arrays.sort(a);
+		    System.out.println(a[2]+a[1]);
+		}
+	}
+}
