d0610cc04316f29ec1f0d0be725f177e97016052
vamsi <bathuyuvaraj@gmail.com>
2026-08-14 11:22:43 +0530

    ✅ IMDB - Rating 1254 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-14

---
+++ codechef/easy/IMDB---Rating-1254/solution.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc= new Scanner(System.in);
+		int t=sc.nextInt();
+		
+		while(t-->0){
+		    int N=sc.nextInt();
+		    int S=sc.nextInt();
+		    
+		    if(S<=N)
+		    System.out.println(S);
+		    
+		    
+		    
+		    else if(S>N)
+		    System.out.println(N-(S-N));
+		}
+	}
+}
