f3503ee08d1974bac953513a82a926a9a56d87d2
vamsi <bathuyuvaraj@gmail.com>
2026-08-18 11:10:36 +0530

    ✅ HASHP02 - Rating 1696 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-18

---
+++ codechef/medium/HASHP02---Rating-1696/solution.java
@@ -0,0 +1,21 @@
+public static int longestPalindromeLength(String s) {
+    Map<Character, Integer> freq = new HashMap<>();
+    for (char ch : s.toCharArray()) {
+        freq.put(ch, freq.getOrDefault(ch, 0) + 1);
+    }
+    
+    int length = 0;
+    boolean oddFound = false;
+    for (int count : freq.values()) {
+        if (count % 2 == 0) {
+            length += count;
+        } else {
+            length += count - 1;
+            oddFound = true;
+        }
+    }
+    if (oddFound) {
+        length += 1;
+    }
+    return length;
+}
