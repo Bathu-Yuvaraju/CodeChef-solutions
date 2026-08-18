653bb5c2cb725167150baa40e14e9b5ffd195f4e
vamsi <bathuyuvaraj@gmail.com>
2026-08-18 11:13:18 +0530

    ✅ PREP51 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-18

---
+++ codechef/medium/PREP51/solution.java
@@ -0,0 +1,65 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        StringBuilder sb = new StringBuilder();
+        while(tc-- > 0){
+            String[] nm = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(nm[0]);
+            int m = Integer.parseInt(nm[1]);
+            String s = br.readLine().trim();
+            String t = br.readLine().trim();
+            
+            String res = minWindowSubstring(s, t);
+            sb.append(res).append("\n");
+        }
+        System.out.print(sb.toString());
+    }
+    
+    private static String minWindowSubstring(String s, String t){
+        int[] need = new int[128], window = new int[128];
+        for (char c: t.toCharArray()){
+            need[c]++;
+        }
+        
+        int required = t.length();
+        int left = 0, minLen = Integer.MAX_VALUE, startIdx = 0;
+        
+        // sliding window for the substring each time hitting a needed match
+        for (int right = 0; right < s.length(); right++){
+            char rChar = s.charAt(right);
+            // if this character is needed then include it in window
+            if(need[rChar] > 0){
+                window[rChar]++;
+                // if the count in window is <= need then we've found one required instance
+                if(window[rChar] <= need[rChar]){
+                    required--;
+                }
+            }
+            
+            // when we have a valid window, try and contract it
+            while(required == 0){
+                // update the result if the current window is smaller
+                if(right - left + 1 < minLen){
+                    minLen = right - left + 1;
+                    startIdx = left;
+                }
+                char lChar = s.charAt(left);
+                if(need[lChar] > 0){
+                    window[lChar]--;
+                    // Once window count drops below needed count, increment required
+                    if(window[lChar] < need[lChar]){
+                        required++;
+                    }
+                }
+                left++;
+            }
+        }
+        
+        return minLen == Integer.MAX_VALUE ? "-1" : s.substring(startIdx, startIdx + minLen);
+    }
+}
