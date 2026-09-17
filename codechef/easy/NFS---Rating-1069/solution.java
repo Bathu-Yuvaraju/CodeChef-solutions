ff969ac671dcb6cbc379f4e0d7bc1b10fcdf08dd
vamsi <bathuyuvaraj@gmail.com>
2026-09-17 10:52:32 +0530

    ✅ NFS - Rating 1069 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-17

---
+++ codechef/easy/NFS---Rating-1069/solution.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc =new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int i=0;i<t;i++)
+        {   
+            int s=sc.nextInt();
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int c=sc.nextInt();
+            s=s*(c+100)/100;
+            
+            if(s>=a && s<=b)
+            System.out.println("yes");
+            else
+            System.out.println("no");
+        }
+		
+	}
+}
