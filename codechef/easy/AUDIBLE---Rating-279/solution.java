ba9baaba3719ba0689c91a40c0ad25c46e97885f
vamsi <bathuyuvaraj@gmail.com>
2026-09-23 20:16:43 +0530

    ✅ AUDIBLE - Rating 279 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-23

---
+++ codechef/easy/AUDIBLE---Rating-279/solution.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+		    int x=sc.nextInt();
+		    if(x>=67&&x<=45000)
+		    {
+		        System.out.println("yes");
+		    }
+		    else
+		    {
+		        System.out.println("no");
+		    }
+		}
+
+	}
+}
