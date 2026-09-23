447fd8fb5057f681fa76771bda247f539e3bc25f
vamsi <bathuyuvaraj@gmail.com>
2026-09-23 20:20:25 +0530

    ✅ BNE_APT - Rating 279 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-23

---
+++ codechef/easy/BNE_APT---Rating-279/solution.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		
+		    int x=sc.nextInt();
+		    if(x>=6&&x<=8)
+		    {
+		        System.out.println("yes");
+		    }
+		    else
+		    {
+		        System.out.println("no");
+		    }
+
+	}
+}
