02662c7c0595a2b2192abf924cc121b79767759b
vamsi <bathuyuvaraj@gmail.com>
2026-09-23 20:22:46 +0530

    ✅ CHEFONDATE - Rating 289 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-23

---
+++ codechef/easy/CHEFONDATE---Rating-289/solution.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+		    int x=sc.nextInt();
+		    if(x%3==0)
+		    {
+		        System.out.println("yes");
+		    }
+		    else
+		    {
+		        System.out.println("no");
+		    }
+		}
+
+
+
+	}
+}
