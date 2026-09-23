ca0b2e6d1a7bc9bfe8c5eeea3648b9083fed8317
vamsi <bathuyuvaraj@gmail.com>
2026-09-23 20:15:41 +0530

    ✅ OFFBY1 - Rating 271 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-23

---
+++ codechef/easy/OFFBY1---Rating-271/solution.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int sum=A+B;
+		int c =(sum * 10) +1;
+		System.out.println(c);
+
+	}
+}
