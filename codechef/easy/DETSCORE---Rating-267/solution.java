256005769dabba96a9b72d65e6616a88676a4468
vamsi <bathuyuvaraj@gmail.com>
2026-09-23 20:15:04 +0530

    ✅ DETSCORE - Rating 267 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-23

---
+++ codechef/easy/DETSCORE---Rating-267/solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int T =sc.nextInt();
+		while(T-->0)
+		{
+		    	int x =sc.nextInt();
+		    	int n =sc.nextInt();
+		    	System.out.println((x / 10) * n);
+		}
+
+	}
+}
